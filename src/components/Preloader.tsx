c67da4969f034bb7d995c4d07878f97875a53104
Andrew <andrew@fariamedia.com>
2019-12-02 15:23:00 -0500

    Added simple Preloader component

---
+++ src/components/Preloader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Preloader: React.FC = () => {
+  return (
+    <div className="preloader">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Preloader;
