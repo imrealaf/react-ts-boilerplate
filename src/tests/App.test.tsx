f17559d535b37e2946168656158310073702d69e
Andrew <andrew@fariamedia.com>
2019-12-02 20:40:32 -0500

    Added App tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "../components/App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App initUser={null} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("<App />", () => {
+  const app = (
+    <Router>
+      <App />
+    </Router>
+  );
+
+  it("renders without crashing", () => {
+    const wrapper = mount(app);
+    wrapper.unmount();
+  });
+
+  it("receives location prop from route and sets data-attribute to DOM", () => {
+    const wrapper = shallow(app);
+    setTimeout(() => {
+      expect(wrapper.prop("location")).toBeInstanceOf("object");
+      expect(document.body.dataset.route).toBeInstanceOf("string");
+    });
+  });
 });
