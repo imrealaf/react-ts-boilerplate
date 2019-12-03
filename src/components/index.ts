eaa29802a08edfcdf4f3b59d9f19f7a38c22e03a
Andrew <andrew@fariamedia.com>
2019-12-02 20:57:43 -0500

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+/**
+ *  Components
+ *
+ *  @type index
+ *  @desc this file just exports all components into one object so they can be imported easier
+ */
+
+import App from "./App";
+import Preloader from "./Preloader";
+
+export { App, Preloader };
